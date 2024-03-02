9dfca67db2a660d688f88acd49af553dd7d4991f
Your Name <you@example.com>
2024-03-02 20:51:21 -0300

    archivos temporales

--- Objetos/obj/Debug/net8.0/Objetos.AssemblyInfo.cs
+++ Objetos/obj/Debug/net8.0/Objetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a0a353b61c4c545c957a21177776e98e4a25d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0246e295b853ff31fb070f4397ff4755e253293")]
 [assembly: System.Reflection.AssemblyProductAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Objetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
